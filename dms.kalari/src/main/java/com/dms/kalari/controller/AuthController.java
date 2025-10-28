99af126490aacb863f3eb148748ae4048675d9ad
Noufal S <noufalnow@gmail.com>
2025-10-28 21:23:25 +0530

    Removed error controller

--- dms.kalari/src/main/java/com/dms/kalari/controller/AuthController.java
+++ dms.kalari/src/main/java/com/dms/kalari/controller/AuthController.java
@@ -29,23 +29,4 @@ public class AuthController {
 		return "access-denied"; // must match the template name
 	}
 
-	@RequestMapping("/error")
-	public String handleError(HttpServletRequest request, Model model) {
-		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-		int statusCode = (status != null) ? Integer.parseInt(status.toString()) : 500;
-
-		String message;
-		switch (statusCode) {
-		case 400 -> message = "Bad request. Please check your input.";
-		case 401 -> message = "You are not authorized to access this page.";
-		case 403 -> message = "Access denied. You don’t have permission to view this page.";
-		case 404 -> message = "The page you’re looking for doesn’t exist.";
-		case 500 -> message = "Internal server error. Please try again later.";
-		default -> message = "An unexpected error occurred.";
-		}
-
-		model.addAttribute("status", statusCode);
-		model.addAttribute("message", message);
-		return "error"; // Maps to error.html template
-	}
 }
